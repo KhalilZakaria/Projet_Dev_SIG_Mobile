9398e7b8e6b79cec84dfaba9c358b18aab75dc44
rdl123 <ramirachid316@gmail.com>
2020-12-27 18:24:27 +0100

    fetched data and adding it to firestore

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,17 +1,46 @@
-import React from 'react';
+import React, {useContext, useState,useEffect }  from 'react';
 import {View, Text, StyleSheet, FlatList} from 'react-native';
+import firestore from '@react-native-firebase/firestore';
 
-
+import { AuthContext } from '../navigation/AuthProvider';
 import {
-  Container,
+  Container, UserImg,
 } from '../styles/FeedStyles';
 
 
 const HomeScreen = () => {
+  const {user, logout} = useContext(AuthContext);
+  const [data, setData] = useState({
+    Description:'',
+    Lat:'',
+    Long:'',
+    user_id:''
+});
+   
+  useEffect(() => {
+      firestore()
+      .collection('Users')
+      .where('user_id', '==', user.uid)
+      .get()
+      .then(function(querySnapshot) {
+        querySnapshot.forEach(function(doc) {
+          setData(doc.data())
+          console.log(data)
+        
+      });
+       
+      });
+  }, [data]);
   return (
+  
     <Container>
-
+   
+                <Text>User id:{data.user_id}</Text>
+                 <Text>Description:{data.Description}</Text>
+                 <Text>Lat:{data.Lat}</Text>
+                 <Text>Long:{data.Long}</Text>
     </Container>
+    
   );
 };
 
