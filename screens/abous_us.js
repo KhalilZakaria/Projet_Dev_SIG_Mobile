a5b811f347d10a9b77d918f9d9e80ae15746286c
Ayoubzoldick <ayoubkhodar1998@gmail.com>
2020-12-29 23:50:57 +0100

    long lat rectif

--- screens/abous_us.js
+++ screens/abous_us.js
@@ -10,6 +10,21 @@ const abous_us = () => {
     );
 };
 
+  // const getData = () => {
+  //   // Initialize the module (needs to be done only once)
+  //   Geocoder.init("AIzaSyCHhPKty4ak8bf8h7hAEcYX-d0NPh3CPHI"); // use a valid API key
+  //   // With more options
+  //   // Geocoder.init("xxxxxxxxxxxxxxxxxxxxxxxxx", {language : "en"}); // set the language
+
+  //   // Search by geo-location (reverse geo-code)
+  //   Geocoder.from(41.89, 12.49)
+  //   .then(json => {
+  //           var addressComponent = json.results[0].address_components[0];
+  //     console.log(addressComponent);
+  //   })
+  //   .catch(error => console.warn(error));
+  // };
+
 export default abous_us;
 
 const styles = StyleSheet.create({
